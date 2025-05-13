f1a117b6fe6368d26376f8b8b1714a41114051d9
JOSEFFE  Oliveira <OLIVEJ14@sa.ad.whirlpool.com>
2025-05-13 20:46:02 -0300

    Projeto Java Quarkus

---
+++ src/main/java/org/acme/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package org.acme.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users3")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer userId;
+
+    private String username;
+
+    public UserEntity() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
+
+
